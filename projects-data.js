6ff7fe9eb2b44d2732d36ed13481b37fbb6782c8
MHMiletic <mmiletic853@gmail.com>
2026-02-27 19:33:45 +0100

    Day one

---
+++ projects-data.js
@@ -0,0 +1,36 @@
+// central data for all projects
+const PROJECTS = [
+  {
+    section: 'mockups',
+    date: '2026-02-25',
+    title: 'Zelda Cook Book',
+    img: 'projects/mockups/COVER_ZELDA_alt.jpg',
+    imgClass: 'cookbook-img',
+    desc: 'Who would not want their waterbottle have a bit more flare.'
+  },
+  {
+    section: 'mockups',
+    date: '2026-02-22',
+    title: 'Mountainrange',
+    img: 'projects/mockups/adventure.png',
+    imgClass: 'mountainrange-img',
+    desc: 'Who would not want a mountain range that follows them around and does their bidding.'
+  },
+  {
+    section: 'mockups',
+    date: '2026-02-20',
+    title: 'BÖTTL',
+    img: 'projects/mockups/bottl.png',
+    imgClass: 'bottl-img',
+    desc: 'Who would not want their waterbottle have a bit more flare.'
+  },
+ {
+    section: 'mockups',
+    date: '2021-02-20',
+    title: 'Movieposter',
+    img: 'projects/mockups/Movieposter.png',
+    imgClass: 'movieposter-img',
+    desc: 'Who would not want their waterbottle have a bit more flare.'
+  }
+  // add more entries as needed, include section values 'socialmedia' or 'personalprojects'
+];
